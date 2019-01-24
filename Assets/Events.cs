a1029cfe2a352e5b2c0b7a6f022ffb19491a19f4
Ed <dkdldzk@gmail.com>
2019-01-24 12:46:09 -0700

    reoriented control axis. have some events already in place.

---
+++ Assets/Events.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Events : MonoBehaviour {
+
+	public Text text;
+	private void OnTriggerEnter(Collider other){
+		text.enabled = true;
+		
+		
+	}
+	
+	private void OnTriggerExit(Collider other){
+		text.enabled = false;
+	}
+}
--- Assets/control.cs
+++ Assets/control.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class control : MonoBehaviour {
 
-	public float mSpeed = 0.5f;
+	public float mSpeed = 0.35f;
+	public float angleSpeed = 15;
 	Vector3 mVelocity;
 	// Use this for initialization
 	void Start () {
@@ -15,24 +16,24 @@ public class control : MonoBehaviour {
 	void Update () {
 	mVelocity = Vector3.zero;
 	if(Input.GetKey(KeyCode.W)){
-		transform.Translate(0.0f,0.0f, mSpeed*Time.deltaTime);
+		transform.Translate(-mSpeed*Time.deltaTime,0.0f,0.0f);
 	}else if(Input.GetKey(KeyCode.S)){
-		transform.Translate(0.0f, 0.0f,-mSpeed*Time.deltaTime);
+		transform.Translate(mSpeed*Time.deltaTime,0.0f, 0.0f);
 	}
 	
 	if(Input.GetKey(KeyCode.D)){
-		transform.Translate(mSpeed*Time.deltaTime,0.0f,0.0f);
+		transform.Translate(0.0f,-mSpeed*Time.deltaTime,0.0f);
 	}else if(Input.GetKey(KeyCode.A)){
-		transform.Translate(-mSpeed*Time.deltaTime,0.0f,0.0f);
+		transform.Translate(0.0f,mSpeed*Time.deltaTime,0.0f);
 		
 	}
 	if(Input.GetKey(KeyCode.E)){
-		transform.Rotate(0.0f,20*Time.deltaTime,0.0f);
+		transform.Rotate(0.0f,0.0f,angleSpeed*Time.deltaTime);
 		
 		
 	}
 	else if(Input.GetKey(KeyCode.Q)){
-		transform.Rotate(0.0f,-20*Time.deltaTime,0.0f);
+		transform.Rotate(0.0f,0.0f,-angleSpeed*Time.deltaTime);
 	}
 	
 	}
