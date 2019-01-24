f3b55a6b38b7177335030f3fd3ad046cf5c9ad62
Ed <dkdldzk@gmail.com>
2019-01-23 17:16:22 -0700

    collision changing text works.

---
+++ Assets/Triggered.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Triggered : MonoBehaviour {
+
+	public Text text;
+	private void OnTriggerEnter(Collider other){
+		text.text = "ENTERED";
+		Debug.Log("ENTERED THE ZONE");
+		
+		
+	}
+	
+	private void OnTriggerExit(Collider other){
+		
+		text.text = "LEFT";
+		Debug.Log("LEFT THE ZONE");
+	}
+}
---
+++ Assets/control.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+	public float mSpeed = 0.5f;
+	Vector3 mVelocity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	mVelocity = Vector3.zero;
+	if(Input.GetKey(KeyCode.W)){
+		transform.Translate(0.0f,0.0f, mSpeed*Time.deltaTime);
+	}else if(Input.GetKey(KeyCode.S)){
+		transform.Translate(0.0f, 0.0f,-mSpeed*Time.deltaTime);
+	}
+	
+	if(Input.GetKey(KeyCode.D)){
+		transform.Translate(mSpeed*Time.deltaTime,0.0f,0.0f);
+	}else if(Input.GetKey(KeyCode.A)){
+		transform.Translate(-mSpeed*Time.deltaTime,0.0f,0.0f);
+		
+	}
+	if(Input.GetKey(KeyCode.E)){
+		transform.Rotate(0.0f,20*Time.deltaTime,0.0f);
+		
+		
+	}
+	else if(Input.GetKey(KeyCode.Q)){
+		transform.Rotate(0.0f,-20*Time.deltaTime,0.0f);
+	}
+	
+	}
+}
